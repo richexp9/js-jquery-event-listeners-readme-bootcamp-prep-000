d8163eeec86188dccad31c319702cd26039d15bf
Richard Loera <richexp9@gmx.com>
2017-12-03 09:22:10 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('i was clicked')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+      $('img').addClass('tasty')
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71){
+        alert('s was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    // if ($( "input:first" ).val() === "correct") {
+    //   alert('your form is going to be submitted now');
+    //   return;
+    // }
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
+  frameIt()
+  pressIt()
+  getIt()
 
 });
